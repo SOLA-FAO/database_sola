269a51d9e4ee21d80e5e69784c129b25dfc160b7
Elton Manoku <emanoku@gmail.com>
2014-09-20 13:21:42 +0200

    Add ba_unit_as_party.rowidentifier [#437 responsible:none status:fixed]

---
+++ changeset/1409b_437.sql
@@ -0,0 +1,5 @@
+﻿ALTER TABLE administrative.ba_unit_as_party 
+  ADD COLUMN rowidentifier character varying(40) not null default uuid_generate_v1();
+
+COMMENT ON COLUMN administrative.ba_unit_as_party.rowidentifier 
+  IS 'SOLA Extension: Identifies the all change records for the row in the ba_unit_as_party table';
