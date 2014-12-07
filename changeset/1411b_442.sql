9cf8daaeabcdf9927addbfdd551e919e73ad6ba5
Alexander Solovov <solovov@gmail.com>
2014-12-07 22:53:27 +0600

    New MapLayerType entity added

---
+++ changeset/1411b_442.sql
@@ -0,0 +1,3 @@
+﻿INSERT INTO system.version SELECT '1411b' WHERE NOT EXISTS (SELECT version_num FROM system.version WHERE version_num = '1411b');
+
+INSERT INTO system.setting(name, vl, active, description) VALUES ('db-utilities-folder', '', 't', 'Full path to PostgreSQL utilities (bin) folder (e.g. C:\Program Files\PostgreSQL\9.1\bin). Used for backup/restore implementation of SOLA Web admin application');
