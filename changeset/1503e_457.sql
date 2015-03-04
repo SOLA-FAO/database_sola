69428886855e4a724c2109bac8109f6f15bad318
Alexander Solovov <solovov@gmail.com>
2015-03-04 15:35:46 +0600

    Language object adjusted to have text  direction attribute.

---
+++ changeset/1503e_457.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO system.version SELECT '1503e' WHERE NOT EXISTS (SELECT version_num FROM system.version WHERE version_num = '1503e');
+
+ALTER TABLE system.language ADD COLUMN ltr boolean NOT NULL DEFAULT 't';
+COMMENT ON COLUMN system.language.ltr IS 'Indicates text direction. If true, then left to right should applied, otherwise right to left.';
+UPDATE system.language SET ltr = 'f' WHERE code = 'ar-JO';
