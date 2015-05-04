2db71877771f5e88aa963a489361e89bbad7d8ba
Alexander Solovov <solovov@gmail.com>
2015-05-04 13:07:02 +0600

    Removing users, roles and groups related to OpenTenure

--- changeset/1504b_463.sql
+++ changeset/1504b_463.sql
@@ -5,4 +5,7 @@ delete from system.setting where name like 'email-msg-user%';
 delete from system.setting where name like 'email-msg-pswd-%';
 delete from system.config_map_layer_metadata where name_layer = 'claims-orthophoto';
 delete from system.config_map_layer where name = 'claims-orthophoto';
+delete from system.approle where code in ('AccessCS','ModerateClaim','RecordClaim','ReviewClaim');
+delete from system.appuser where id in ('claim-moderator','claim-recorder','claim-reviewer');
+delete from system.appgroup where id in ('claim-moderators','claim-reviewers','CommunityMembers','CommunityRecorders');
 drop schema opentenure cascade;
